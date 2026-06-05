cd7a3e4cd0157a5933cb768bae7878bc7fa931fe
SUBANU S <subanu652@gmail.com>
2026-06-06 01:07:28 +0530

    Added LoginPage using page object model

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    private By usernameField = By.name("username");
+    private By passwordField = By.name("password");
+    private By loginButton = By.xpath("//button[@type='submit']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLoginButton();
+    }
+}
